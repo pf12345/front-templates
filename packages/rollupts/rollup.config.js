744bc6c9a0c43dc838a0e8aa5d06c768f500b03b
pengfeng02 <pengfeng02@meicai.cn>
2021-07-26 11:15:43 +0800

    add rollupts

---
+++ packages/rollupts/rollup.config.js
@@ -0,0 +1,39 @@
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import babel  from 'rollup-plugin-babel';
+import typescript from 'rollup-plugin-typescript2';
+import { DEFAULT_EXTENSIONS } from '@babel/core';
+import {terser} from 'rollup-plugin-terser';
+
+let defaults = { compilerOptions: { declaration: true } };
+let override = { compilerOptions: { declaration: false } };
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: './dist/bundle.js',
+    format: 'es',
+    plugins: [terser()]
+  },
+  plugins: [
+    typescript({
+      tsconfigDefaults: defaults,
+      tsconfig: "tsconfig.json",
+      tsconfigOverride: override
+    }),
+    nodeResolve({
+      jsnext: true,
+      main: true
+    }),
+    commonjs({
+      // non-CommonJS modules will be ignored, but you can also
+      // specifically include/exclude files
+      include: 'node_modules/**',  // Default: undefined
+    }),
+    babel({ extensions: [...DEFAULT_EXTENSIONS, '.js', '.ts'], exclude: "node_modules/**" })
+  ],
+  watch: {
+    include: 'src/**',
+    clearScreen: true
+  }
+}
---
+++ packages/rollupts/src/index.ts
@@ -0,0 +1,9 @@
+import add from './util';
+
+const a = 1;
+
+const b = (x:number) => {
+  console.log('11122', x)
+}
+
+console.log('this is test', b(a), add(a));
